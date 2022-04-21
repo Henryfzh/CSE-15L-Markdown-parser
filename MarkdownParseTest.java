79801958d9c62af4953764818e61c3743ff0db3a
Henryfzh <henryfzh@outlook.com>
2022-04-21 08:50:08 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,5 +2,11 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class MarkdownParseTest {
-    
+    // To tell the program this is a test
+    @Test
+    public void addition() {
+        //Evaluate the expectation value 
+        //to the real value
+        assertEquals(2, 1 + 1);
+    }
 }
