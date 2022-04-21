4d4458851961a41fed0407dd4233a5ba9bcd00ab
Henryfzh <henryfzh@outlook.com>
2022-04-21 09:21:56 -0700

    Update MarkdownParseTest.java
    Test passed of test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,10 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -8,5 +14,16 @@ public class MarkdownParseTest {
         //Evaluate the expectation value 
         //to the real value
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testfile() throws IOException {
+        MarkdownParse m = new MarkdownParse();
+        Path path = Path.of("test-file.md");
+        String content = Files.readString(path);
+        List<String> links = m.getLinks(content);
+        List<String>linkList = List.of("https://something.com, some-thing.html");
+        assertEquals(linkList.toString(), links.toString());
+        
     }
 }
